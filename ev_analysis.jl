a4e1654b7e77a8f549f31dfc15b6f62f5da6d9b9
kjxymful <pleibersperger@yahoo.de>
2022-07-01 15:05:41 +0200

    Fix bugs

--- ev_analysis.jl
+++ ev_analysis.jl
@@ -1,11 +1,13 @@
 include("src/empirical_evd.jl") # for ide reasons needs to be removed for run/final version
-using empirical_evd
+using .empirical_evd
 using BPTT
 using Flux
 using NPZ
+using LinearAlgebra
 
 # run file
-if abspath(PROGRAM_load_dataset()FILE) == @__FILE__ 
-    
-    m = load
+function main()
+
 end
+
+main()
